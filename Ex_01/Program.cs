4b2762d3e59523ffd009ddd4bf527dc2edeb2e6a
Andrey_Sim <demenyuk906@gmail.com>
2023-04-11 01:33:35 +0500

    Ex_01

---
+++ Ex_01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите целое число №1: ");
+string UserNamber1 = Console.ReadLine() ?? "";
+int namber_a = int.Parse(UserNamber1);
+
+Console.Write("Введите целое число №2: ");
+string UserNamber2 = Console.ReadLine() ?? "";
+int namber_b = int.Parse(UserNamber2);
+
+if (namber_a > namber_b)
+{
+    Console.Write($"Max Namber = {namber_a}, Min Namber = {namber_b}.");
+}
+else
+{
+    Console.Write($"Max Namber = {namber_b}, Min Namber = {namber_a}.");
+}

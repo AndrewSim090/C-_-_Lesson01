7aa9e26ac68ec06f016a5de0673c7340c981f43a
Andrey_Sim <demenyuk906@gmail.com>
2023-04-11 03:21:49 +0500

    Ex_03

---
+++ Ex_03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число: ");
+string UserNamber1 = Console.ReadLine() ?? "";
+int namber_a = int.Parse(UserNamber1);
+
+if (namber_a % 2 == 0)
+{
+    Console.Write("четное");
+}
+else
+{
+    Console.Write("не четное");
+}

6665d19a3fabd52e2d4c370945eb94e7a49f3c0b
Andrey_Sim <demenyuk906@gmail.com>
2023-04-12 16:47:13 +0500

    Ex_04

---
+++ Ex_04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+string UserNamber1 = Console.ReadLine() ?? "";
+int namber_a = int.Parse(UserNamber1);
+
+int count = 2;
+
+while (count <= namber_a)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
